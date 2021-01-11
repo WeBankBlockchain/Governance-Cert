d27773bbd31a07202dc1bdc382b3935f2d385103
卡拉马佐夫 <973459080@qq.com>
2021-01-11 17:47:34 +0800

    add key sm2

--- cert-toolkit/src/main/java/com/webank/cert/toolkit/utils/KeyUtils.java
+++ cert-toolkit/src/main/java/com/webank/cert/toolkit/utils/KeyUtils.java
@@ -17,10 +17,22 @@ package com.webank.cert.toolkit.utils;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
+import org.bouncycastle.crypto.params.ECDomainParameters;
+import org.bouncycastle.crypto.params.ECKeyGenerationParameters;
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.math.ec.custom.gm.SM2P256V1Curve;
 import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemWriter;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -28,7 +40,9 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.security.interfaces.ECPrivateKey;
+import java.security.spec.ECGenParameterSpec;
 import java.security.spec.ECParameterSpec;
 import java.security.spec.ECPoint;
 import java.security.spec.ECPublicKeySpec;
@@ -46,6 +60,19 @@ import java.util.Collections;
 @Slf4j
 public class KeyUtils {
 
+	public static final SM2P256V1Curve CURVE = new SM2P256V1Curve();
+	public final static BigInteger SM2_ECC_GX = new BigInteger(
+			"32C4AE2C1F1981195F9904466A39C9948FE30BBFF2660BE1715A4589334C74C7", 16);
+	public final static BigInteger SM2_ECC_GY = new BigInteger(
+			"BC3736A2F4F6779C59BDCEE36B692153D0A9877CC62A474002DF32E52139F0A0", 16);
+	public final static BigInteger SM2_ECC_N = CURVE.getOrder();
+	public final static BigInteger SM2_ECC_H = CURVE.getCofactor();
+	public static final org.bouncycastle.math.ec.ECPoint G_POINT = CURVE.createPoint(SM2_ECC_GX, SM2_ECC_GY);
+	public static final ECDomainParameters DOMAIN_PARAMS = new ECDomainParameters(CURVE, G_POINT,
+			SM2_ECC_N, SM2_ECC_H);
+	private static final String PEM_STRING_ECPRIVATEKEY = "EC PRIVATE KEY";
+
+	private static final SecureRandom random = new SecureRandom();
 
 	public static KeyPair generateKeyPair(){
 		KeyPairGenerator keyPairGen = null;
@@ -53,11 +80,84 @@ public class KeyUtils {
 		try {
 			keyPairGen = KeyPairGenerator.getInstance("RSA");
 			keyPairGen.initialize(2048);
-			keyPair =  keyPairGen.generateKeyPair();
+			keyPair = keyPairGen.generateKeyPair();
 		} catch (NoSuchAlgorithmException e) {
 			log.error("generateKeyPair failed", e);
 		}
 		return keyPair;
+	}
+
+	/**
+	 * Generate ECDSA key pairs
+	 */
+	public static KeyPair generateECDSAKeyPair() throws NoSuchProviderException, NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA", "BC");
+        ECGenParameterSpec ecGenParameterSpec = new ECGenParameterSpec("secp256k1");
+        keyPairGenerator.initialize(ecGenParameterSpec, random);
+        return keyPairGenerator.generateKeyPair();
+	}
+
+	/**
+	 * Generate SM2 key pairs
+	 */
+	public static KeyPair generateSM2KeyPair() throws NoSuchProviderException, NoSuchAlgorithmException,
+			InvalidAlgorithmParameterException {
+		return generateECKeyPair(DOMAIN_PARAMS, random);
+	}
+
+	public static AsymmetricCipherKeyPair generateSM2KeyPairParameter() {
+		return generateKeyPairParameter(DOMAIN_PARAMS, random);
+	}
+
+	public static KeyPair generateECKeyPair(ECDomainParameters domainParameters, SecureRandom random)
+			throws NoSuchProviderException, NoSuchAlgorithmException,
+			InvalidAlgorithmParameterException {
+		KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC", BouncyCastleProvider.PROVIDER_NAME);
+		org.bouncycastle.jce.spec.ECParameterSpec parameterSpec = new org.bouncycastle.jce.spec.ECParameterSpec(
+				domainParameters.getCurve(), domainParameters.getG(),
+				domainParameters.getN(), domainParameters.getH());
+		kpg.initialize(parameterSpec, random);
+		return kpg.generateKeyPair();
+	}
+
+	/**
+	 * Generate ECC key pairs
+	 */
+	public static AsymmetricCipherKeyPair generateKeyPairParameter(
+			ECDomainParameters domainParameters, SecureRandom random) {
+		ECKeyGenerationParameters keyGenerationParams = new ECKeyGenerationParameters(domainParameters, random);
+		ECKeyPairGenerator keyGen = new ECKeyPairGenerator();
+		keyGen.init(keyGenerationParams);
+		return keyGen.generateKeyPair();
+	}
+
+	/**
+	 * Converts the PKCS8 standard private key byte stream to a private key pair
+	 */
+	public static BCECPrivateKey convertPKCS8ToECPrivateKey(byte[] pkcs8Key)
+			throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException {
+		PKCS8EncodedKeySpec peks = new PKCS8EncodedKeySpec(pkcs8Key);
+		KeyFactory kf = KeyFactory.getInstance("EC", BouncyCastleProvider.PROVIDER_NAME);
+		return (BCECPrivateKey) kf.generatePrivate(peks);
+	}
+
+	/**
+	 * Converts the PKCS8 standard private key byte stream to PEM
+	 */
+	public static String convertECPrivateKeyPKCS8ToPEM(byte[] encodedKey) throws IOException {
+		return convertEncodedDataToPEM(PEM_STRING_ECPRIVATEKEY, encodedKey);
+	}
+
+	private static String convertEncodedDataToPEM(String type, byte[] encodedData) throws IOException {
+		ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+		PemWriter pWrt = new PemWriter(new OutputStreamWriter(bOut));
+		try {
+			PemObject pemObj = new PemObject(type, encodedData);
+			pWrt.writeObject(pemObj);
+		} finally {
+			pWrt.close();
+		}
+		return new String(bOut.toByteArray());
 	}
 
 	public static PublicKey getRSAPublicKey(String key) throws Exception {
